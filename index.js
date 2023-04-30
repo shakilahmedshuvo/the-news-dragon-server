807d3f74d5a02486d344646ad87bbc230425262b
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-04-30 23:50:59 +0600

    add news.json file data for id

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
@@ -12,6 +13,32 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+})
+
+// news category
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+// news id
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews);
+})
+
+// news category id
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    // console.log(id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews)
+    }
 })
 
 
