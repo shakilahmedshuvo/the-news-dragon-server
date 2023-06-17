fd129574ca10d6d6f9f5c1fd84b9704fe3a0bd0a
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-06-17 12:56:07 +0600

    add Readme file

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const port = process.env.PORT || 5000;
 const categories = require('./data/categories.json');
 const news = require('./data/news.json');
 
+// use middle wars
 app.use(cors());
 
+// home api
 app.get('/', (req, res) => {
     res.send('Dragon is running')
 });
 
+// categories api
 app.get('/categories', (req, res) => {
     res.send(categories);
 })
@@ -40,7 +43,6 @@ app.get('/categories/:id', (req, res) => {
         res.send(categoryNews)
     }
 })
-
 
 app.listen(port, () => {
     console.log(`Dragon API is running on port: ${port}`);
